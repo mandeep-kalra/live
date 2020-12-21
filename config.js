bbcd72bf8db903a792d3611af81b160569228f67
mandeep-kalra <38689594+mandeep-kalra@users.noreply.github.com>
2020-12-21 22:01:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ module.exports = {
     "development":{
         'secretKey': '7HE91-QM30X-ZYKS5-8G8HA-BCW0J-PDN2C',
         'scrt': 'portal\/\/\'*iD',
-        'mongoUrl' : 'mongodb://idiscover:idiscover123@ds229458.mlab.com:29458/app',
+        'mongoUrl' : 'mongodb+srv://idiscover:idiscover123@cluster-app.kutxf.mongodb.net/app?retryWrites=true&w=majority',
+        'mongoUrl_' : 'mongodb://idiscover:idiscover123@ds229458.mlab.com:29458/app',
         'googleAuth' : {
             'clientID'      : '437438716282-4s1rmctjev4njem80m4a12j5fg47u850.apps.googleusercontent.com',
             'clientSecret'  : 'tPkCriCx-Q7hvS8DwauyutOT',
@@ -13,8 +14,9 @@ module.exports = {
     },
     "production":{
         'secretKey': '7HE91-QM30X-ZYKS5-8G8HA-BCW0J-PDN2C',
-        'scrt': 'portal\/\/\'*iD',
-        'mongoUrl' : 'mongodb://idiscover:idiscover123@ds229458.mlab.com:29458/app',
+        'scrt': 'portal\/\/\'*iD',        
+        'mongoUrl' : 'mongodb+srv://idiscover:idiscover123@cluster-app.kutxf.mongodb.net/app?retryWrites=true&w=majority',
+        'mongoUrl_' : 'mongodb://idiscover:idiscover123@ds229458.mlab.com:29458/app',
         'googleAuth' : {
             'clientID'      : '437438716282-4s1rmctjev4njem80m4a12j5fg47u850.apps.googleusercontent.com',
             'clientSecret'  : 'tPkCriCx-Q7hvS8DwauyutOT',
